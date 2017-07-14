40f4ed7ac0c6ae520a2ced0c268f2395519429f7
RaeYoung <lynnyoung@lynnyoung.lan>
2017-07-14 17:34:02 +0800

    Update quishi_spider.py
    Version 2 , update the mini program with multiprocessing

--- qiushi_spider.py
+++ qiushi_spider.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
+# @author Rui Y
+# @version 2
+
 import requests
 from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
-import time
+import time, random
+from multiprocessing import Pool
 
 def get_one_page(url):
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+    }
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers = headers)
         if response.status_code == 200:
             return response.text
-        return None
-    except RequestException:
-        return None
-
-def get_article_page(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.text
+        print(response.status_code)
         return None
     except RequestException:
         return None
@@ -45,23 +45,30 @@ def parse_one_page(html):
         if '查看全文' in content:
             # Get content-id, then can get url
             id = item['id'].replace('qiushi_tag_', '')
-            articlePage = get_article_page('http://www.qiushibaike.com/article/' + str(id))
+            articlePage = get_one_page('http://www.qiushibaike.com/article/' + str(id))
             content = parse_article_content(articlePage)
         yield {
             'name' : name,
             'content' : content
         }
 
-def main(page):
-    url = 'https://www.qiushibaike.com/text/page/' + str(page+1)
+def main(url):
     html = get_one_page(url)
     for joke in parse_one_page(html):
         write_to_file(joke)
 
+
 if __name__ == '__main__':
+    urls = []
+    for i in range(1, 36):
+        urls.append('https://www.qiushibaike.com/text/page/' + str(i) + '/')
     start = time.time()
-    for i in range(35):
-        main(i)
+    p = Pool(10)
+    for url in urls:
+        p.apply_async(main, args=(url, ))
+        time.sleep(random.random()*3)
+    p.close()
+    p.join()
     end = time.time()
     time = end - start
     with open('jokeResult.txt', 'a') as f:
